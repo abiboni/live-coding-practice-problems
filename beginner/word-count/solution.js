a82f7b2f9a53de2aa02f7ea6544c691d3947abae
abiboni <abigail.o.bonilla@gmail.com>
2023-12-06 20:48:23 -0500

    git commands

--- beginner/word-count/solution.js
+++
@@ -1,3 +0,0 @@
-/* 
-  Switch to the solution branch to view one or more possible solutions... after you've come up with your own, of course!
-*/
--- intermediate/train-travel/practice.js
+++
@@ -1,5 +0,0 @@
-/* 
-  Practice live coding!
-  See repository README for general instructions.
-  See prompt.md in this directory for specific instructions.
-*/
